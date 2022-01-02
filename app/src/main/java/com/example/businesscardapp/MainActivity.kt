b84551ca7d0dfa3b55bfd365ff2eae23a632fdaa
readbak <ridwan.bakare@babbangona.com>
2022-01-02 10:39:52 +0100

    Unversioned files

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     CreateBusinessCard()
+
                 }
             }
         }
